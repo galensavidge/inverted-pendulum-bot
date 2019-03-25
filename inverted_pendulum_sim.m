cca011bcc617db287d66e3832410001db08ceaa0
Galen Savidge <gsavidge@ucsc.edu>
2019-03-25 02:36:57 -0700

    Position control works, PD needs tuning

--- inverted_pendulum_sim.m
+++ inverted_pendulum_sim.m
@@ -9,8 +9,9 @@ close all
 % Simulation parameters
 dt = 0.01; % Seconds
 sim_time = 10; % Seconds
-B_desired = 1; % Meters
+B_desired = 5; % Meters
 % pitch_desired = 0; % Radians
+max_speed = 1; % m/s
 
 % Graphing parameters
 body_color = [0.1 0.1 0.8];
@@ -19,7 +20,7 @@ wheel_color = [0.8 0.1 0.1];
 % Initialize simulated physical system
 r_wheel = 0;
 l_wheel = 0;
-pitch = pi/6; % Radians
+pitch = 0; % Radians
 w = 0; % Rad/s
 w_dot = 0; % Rad/s^2
 yaw = 0;
@@ -44,20 +45,14 @@ t = 0:dt:(sim_time-dt);
 % High level feedback controller
 K_pos = 0.15;
 Kp_pos = 1*K_pos;
-Ki_pos = 0*K_pos;
 Kd_pos = 1*K_pos;
-max_pos_err = 0.5;
-
-B = [W - r*sin(pitch); wheel_radius + r*cos(pitch)];
-last_pos_err = B(1) - B_desired;
-
-pos_integrator = 0;
 
 % Run high level controller for the first step
-pos_err = B(1) - B_desired;
+B = [W - r*sin(pitch); wheel_radius + r*cos(pitch)];
 
-pos_integrator = pos_integrator + Ki_pos*pos_err*dt;
-c_pos = Kp_pos*pos_err + Kd_pos*(pos_err - last_pos_err)/dt + pos_integrator;
+pos_err = B(1) - B_desired;
+pos_err_p = sign(pos_err)*min(abs(pos_err), max_speed*Kd_pos/Kp_pos);
+c_pos = Kp_pos*pos_err_p;
 last_pos_err = pos_err;
 
 pitch_desired = c_pos;
@@ -73,7 +68,7 @@ pitch_integrator = 0;
 
 % History for graphing
 pitch_hist = zeros(1, num_steps);
-W_hist = zeros(1, num_steps);
+v_W_hist = zeros(1, num_steps);
 w_W_hist = zeros(1, num_steps);
 
 % Set up real time plot
@@ -89,8 +84,8 @@ title('Inverted Pendulum Bot Simulation')
 for i = 1:num_steps
     % High level controller
     pos_err = B(1) - B_desired;
-    pos_integrator = pos_integrator + Ki_pos*pos_err*dt;
-    c_pos = Kp_pos*pos_err + Kd_pos*(pos_err - last_pos_err)/dt + pos_integrator;
+    pos_err_p = sign(pos_err)*min(abs(pos_err), max_speed*Kd_pos/Kp_pos);
+    c_pos = Kp_pos*pos_err_p + Kd_pos*(pos_err - last_pos_err)/dt;
     last_pos_err = pos_err;
     
     pitch_desired = c_pos;
@@ -113,7 +108,8 @@ for i = 1:num_steps
     v_W = v_W + v_W_dot*dt;
     W = W + v_W*dt;
     
-    w_W_hist(i) = -v_W/wheel_radius;
+    v_W_hist(i) = v_W;
+    w_W_hist(i) = -v_W/wheel_radius - w_dot;
     
     % Find body position
     B = [W - r*sin(pitch); wheel_radius + r*cos(pitch)];
@@ -125,7 +121,6 @@ for i = 1:num_steps
     rectangle('Position',[W(1) - wheel_radius, 0 , 2*wheel_radius, 2*wheel_radius],...
     'Curvature',[1,1], 'FaceColor',wheel_color);
     plot([B(1)-1 B(1)+1], [0, 0], 'k')
-    pause(0.001)
 end
 
 figure(2)
@@ -136,8 +131,15 @@ ylabel('Pitch (degrees)')
 grid on
 
 figure(3)
+plot(t, v_W_hist)
+title('Wheel translational speed over time')
+xlabel('Time (s)')
+ylabel('Wheel speed (m/s)')
+grid on
+
+figure(4)
 plot(t, w_W_hist)
-title('Wheel speed over time')
+title('Wheel angular velocity over time')
 xlabel('Time (s)')
 ylabel('Wheel speed (rad/s)')
 grid on
